7b88f94ab6b0e40b5f1e62899be14886140b38c3
Alexander Glenn <aglenn@findawayworld.com>
2014-03-06 17:11:58 -0500

    now accepts HTTP requests. Need to tie them together

--- gifServer.py
+++ gifServer.py
@@ -1,11 +1,14 @@
 from autobahn.twisted.websocket import WebSocketServerProtocol, WebSocketServerFactory
+from twisted.web import server, resource
+from twisted.internet import reactor
 import json
 import time
 
-class MyServerProtocol(WebSocketServerProtocol):
+class SocketProtocol(WebSocketServerProtocol):
 
    def onConnect(self, request):
       print("Client connecting: {0}".format(request.peer))
+      sender = self;
 
    def onOpen(self):
       print("WebSocket connection open.")
@@ -18,6 +21,15 @@ class MyServerProtocol(WebSocketServerProtocol):
    def onClose(self, wasClean, code, reason):
       print("WebSocket connection closed: {0}".format(reason))
 
+   def blah():
+      print("Blah")
+
+class WebResource(resource.Resource):
+    isLeaf = True
+    def render_GET(self, request):
+        return "gifFactory"
+    def render_POST(self, request):
+      return json.dumps({"status":"1"})
 
 
 if __name__ == '__main__':
@@ -30,7 +42,11 @@ if __name__ == '__main__':
    log.startLogging(sys.stdout)
 
    factory = WebSocketServerFactory("ws://localhost:9000", debug = False)
-   factory.protocol = MyServerProtocol
+   factory.protocol = SocketProtocol
+   factory.protocol.blah();
+   
+   site = server.Site(WebResource())
 
+   reactor.listenTCP(8080, site)
    reactor.listenTCP(9000, factory)
    reactor.run()
