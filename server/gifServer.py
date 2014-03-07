c42a8e5fa343288e49f7f6457c49f8994b280a07
Alex Glenn <aglenn88@gmail.com>
2014-03-06 23:10:07 -0500

    Server now takes the posted url, downloads it, and sends it to all clients

--- server/gifServer.py
+++ server/gifServer.py
@@ -3,6 +3,7 @@ from twisted.web import server, resource
 from twisted.internet import reactor
 import json
 import time
+import os
 
 class SocketProtocol(WebSocketServerProtocol):
 
@@ -12,6 +13,7 @@ class SocketProtocol(WebSocketServerProtocol):
 
    def onOpen(self):
       print("WebSocket connection open.")
+      self.factory.register(self)
 
    def onMessage(self, payload, isBinary):
       print("Message received: {0}".format(payload.decode('utf8')))
@@ -21,14 +23,39 @@ class SocketProtocol(WebSocketServerProtocol):
    def onClose(self, wasClean, code, reason):
       print("WebSocket connection closed: {0}".format(reason))
 
-   def blah():
-      print("Blah")
+class PostableSocketFactory(WebSocketServerFactory):
+
+   def __init__(self, url, debug = False, debugCodePaths = False):
+      WebSocketServerFactory.__init__(self, url, debug = debug, debugCodePaths = debugCodePaths)
+      self.clients = []
+
+   def register(self, client):
+      if not client in self.clients:
+         print("registered client {}".format(client.peer))
+         self.clients.append(client)
+
+   def unregister(self, client):
+      if client in self.clients:
+         print("unregistered client {}".format(client.peer))
+         self.clients.remove(client)
+
+   def broadcast(self, msg):
+      print("broadcasting message '{}' ..".format(json.dumps(msg)))
+      print("To " + str(len(self.clients)) + " clients")
+      for c in self.clients:
+         c.sendMessage(json.dumps(msg))
+         print("message sent to {}".format(c.peer))
+
 
 class WebResource(resource.Resource):
     isLeaf = True
     def render_GET(self, request):
         return "gifFactory"
     def render_POST(self, request):
+      jresult = json.loads(request.content.read())
+      factory.broadcast(jresult)
+      command = "curl " + jresult["url"] + " > gifs/" + jresult["url"].split("/")[-1]
+      os.system(command)
       return json.dumps({"status":"1"})
 
 
@@ -39,11 +66,12 @@ if __name__ == '__main__':
    from twisted.python import log
    from twisted.internet import reactor
 
+   os.system("mkdir gifs")
+
    log.startLogging(sys.stdout)
 
-   factory = WebSocketServerFactory("ws://localhost:9000", debug = False)
+   factory = PostableSocketFactory("ws://localhost:9000", debug = False)
    factory.protocol = SocketProtocol
-   factory.protocol.blah();
    
    site = server.Site(WebResource())
 
